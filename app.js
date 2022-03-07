a2bc3fea3a7e9f909e7f41fd63e3f2432d949d9c
Maddy <madisonschaaf48@gmail.com>
2022-03-07 16:45:07 -0700

    fixed changes to js

--- app.js
+++ app.js
@@ -1,16 +1,14 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs)
+const fs = require('fs');
+const generatePage = require('./src/page-template')
 
-const printProfileData = profileDataArr => {
-    //this
-    for(let i = 0; i < profileDataArr.length; i+= 1) {
-        console.log(profileDataArr[i]);    
-    }
-    console.log('============================');
-    //is the same as this..
-    profileDataArr.forEach((profileItem)=>
-        console.log(profileItem))
-    
-};
+const profileDataArgs = process.argv.slice(2);
 
-printProfileData(profileDataArgs);
+const [name, github]= profileDataArgs;
+
+const pageHTML = generatePage(name, github);
+
+fs.writeFile('./index.html', pageHTML, err => {
+  if (err) throw err;
+
+  console.log('Portfolio complete! Check out index.html to see the output!')
+})
