4514fb6e69678c724c07146d040ae5a390961868
Maddy <madisonschaaf48@gmail.com>
2022-03-09 09:40:02 -0700

    added validation to answers

--- app.js
+++ app.js
@@ -7,12 +7,27 @@ return inquirer.prompt([
   {
     type: 'input',
     name: 'name',
-    message: 'What is your name?'
+    message: 'What is your name? (Required)',
+    validate: nameInput=> {
+      if (nameInput) {
+      return true;
+    } else {
+      console.log('Please enter your name!')
+      return false;
+    }
+  }
   },
   {
     type: 'input',
     name: 'github',
-    message: 'Enter your GitHub Username'
+    message: 'Enter your GitHub Username (Required)',
+    validate: userInput => {
+      if(userInput) {
+        return true;
+      } else {
+        console.log('Please enter your username!')
+      }
+    }
   },
   {
     type: 'input',
@@ -38,12 +53,26 @@ const promptProject = portfolioData => {
     {
       type:'input',
       name: 'name',
-      message: 'What is the name of your project?'
+      message: 'What is the name of your project? (Required)',
+      validate: nameInput => {
+        if(nameInput) {
+          return true;
+        } else {
+          console.log('Please enter the name of your project!')
+        }
+      }
     },
     {
       type: 'input',
       name: 'description',
-      message: 'Provide a description of the project(Required)'
+      message: 'Provide a description of the project(Required)',
+      validate: description => {
+        if (description) {
+          return true;
+        } else {
+          console.log('Please enter a desription of the project')
+        }
+      }
     },
     {
       type: 'checkbox',
@@ -54,8 +83,15 @@ const promptProject = portfolioData => {
     {
       type: 'input',
       name: 'link',
-      message: 'Enter the GitHub link to your project. (Required)'
-    },
+      message: 'Enter the GitHub link to your project. (Required)',
+      validate: link => {
+        if (link) {
+          return true; 
+          } else {
+            console.lof('Please enter GitHub link')
+          }
+        }
+      },
     {
       type: 'confirm',
       name: 'feature',
